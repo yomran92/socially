9defd5c9f58dc229b4a7c0c7871e47e0192b0a71
UP2Media <123948719+uptomedia@users.noreply.github.com>
2024-08-22 14:06:22 +0300

    add _post deigne_link to api

--- lib/features/post/domain/repositories/post_repository.dart
+++ lib/features/post/domain/repositories/post_repository.dart
@@ -1,5 +1,4 @@
 import 'package:dartz/dartz.dart';
-import 'package:todoapp/features/task/data/models/param/delete_task_param.dart';
 
 import '../../../../core/error/error_entity.dart';
 import '../../data/models/param/get_all_post_param.dart';
--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -12,6 +12,10 @@ import 'features/home/data/data_sources/remote/remote_data_source.dart';
 import 'features/home/data/repositories/default_story_repository.dart';
 import 'features/home/domain/repositories/story_repository.dart';
 import 'features/home/presentation/bloc/story/story_bloc.dart';
+import 'features/post/data/data_sources/remote/remote_data_source.dart';
+import 'features/post/data/repositories/default_post_repository.dart';
+import 'features/post/domain/repositories/Post_repository.dart';
+import 'features/post/presentation/bloc/post_bloc.dart';
 
 
 final sl = GetIt.instance;
@@ -31,15 +35,23 @@ Future<void> init() async {
 
   sl.registerFactory(() => HiveParamter(path: directory.path, hive: sl()));
    sl.registerLazySingleton(() => StoryBloc());
+  sl.registerLazySingleton(() => PostBloc());
 
   sl.registerLazySingleton(() => NetworkInfo());
   sl.registerLazySingleton(() => AppStateModel());
+  sl.registerLazySingleton(() =>PostRemoteDataSource(
+    sl(),
+  ));
+  sl.registerLazySingleton<PostRepository>(() => DefaultPostRepository(sl()));
 
   // Data sources
 
       sl.registerLazySingleton(() => StoryRemoteDataSource(
         sl(),
       ));
+  sl.registerLazySingleton(() =>PostRemoteDataSource(
+    sl(),
+  ));
 
   // Repositories
    sl.registerLazySingleton<StoryRepository>(() => DefaultStoryRepository(sl()));
